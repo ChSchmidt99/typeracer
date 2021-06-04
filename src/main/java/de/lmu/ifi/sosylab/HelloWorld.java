af73a864d30ca93752b2f7c60ef9484f4a319e70
Thomas Lemberger <lembergerth@gmail.com>
2021-06-04 09:58:55 +0200

    Add JavaDoc to HelloWorld
    So that 'gradle javadoc' generates some better HTML example

--- src/main/java/de/lmu/ifi/sosylab/HelloWorld.java
+++ src/main/java/de/lmu/ifi/sosylab/HelloWorld.java
@@ -1,7 +1,15 @@
 package de.lmu.ifi.sosylab;
 
+/**
+ * Application that prints a nice and warm hello on invocation.
+ * This class ignores any command-line arguments.
+ */
 public class HelloWorld {
 
+  /**
+   * Runs the application.
+   * @param args ignored
+   */
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
