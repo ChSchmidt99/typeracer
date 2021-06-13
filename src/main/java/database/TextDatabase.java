06d8d22b5911e5880d167e5aea840180a0c424ff
Chris on Mac <chn.schmidt23@gmail.com>
2021-06-13 13:03:00 +0200

    added database package to sourceSets

--- src/main/java/database/TextDatabase.java
+++ src/main/java/database/TextDatabase.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class TextDatabase {
@@ -9,22 +10,30 @@ public class TextDatabase {
 
   public TextDatabase() throws FileNotFoundException {
     dictionary = new ArrayList<String>();
-    String path = TextDatabase.class.getResource("dictionary.txt").getPath();
+    URL path = this.getClass().getClassLoader().getResource("dictionary.txt");
+    if (path == null) {
+      throw new FileNotFoundException();
+    }
+
+    System.out.println(path.getPath());
 
     //        Scanner s = new Scanner(new File(path), "UTF-8");
     //        while (s.hasNextLine()) {
     //            dictionary.add(s.next());
     //        }
     //        s.close();
-    System.out.println(path);
   }
 
   public ArrayList<String> getDictionary() {
     return dictionary;
   }
 
-  public static void main(String[] args) throws FileNotFoundException {
-    TextDatabase t = new TextDatabase();
-    System.out.println(t.getDictionary());
+  public static void main(String[] args) {
+    try {
+      TextDatabase t = new TextDatabase();
+      System.out.println(t.getDictionary());
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 }
