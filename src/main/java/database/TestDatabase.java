7b6178f841bffdc3660288ca2bd1e22d8ebae8e2
fadeetch <fadejeff@gmail.com>
2021-06-20 16:46:42 +0200

    Trying to onboard database.txt

--- src/main/java/database/TestDatabase.java
+++ src/main/java/database/TestDatabase.java
@@ -1,8 +1,10 @@
 package database;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class TestDatabase implements Database {
@@ -16,14 +18,15 @@ public class TestDatabase implements Database {
 
   @Override
   public String registerUser(String username) throws IOException {
-    Path path = Paths.get(String.valueOf(this.getClass().getResource("database.txt")));
+    // String path = this.getClass().getClassLoader().getResource("dictionary.txt").getPath();
+    String path = this.getClass().getClassLoader().getResource("database.txt").getPath();
 
-    //    Files.writeString(
-    //        Paths.get(String.valueOf(path)),
-    //
-    //        (username + System.lineSeparator())
-    //        StandardCharsets.UTF_8,
-    //        StandardOpenOption.APPEND);
+    Files.writeString(
+        Paths.get(path),
+        (username + System.lineSeparator()),
+        StandardCharsets.UTF_8,
+        StandardOpenOption.APPEND);
+
     return path.toString();
   }
 
@@ -35,5 +38,6 @@ public class TestDatabase implements Database {
   public static void main(String[] args) throws IOException {
     TestDatabase m = new TestDatabase();
     System.out.println(m.registerUser("amazing dude"));
+    System.out.println(m.registerUser("some stuff"));
   }
 }
